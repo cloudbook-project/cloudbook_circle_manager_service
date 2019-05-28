05c27e39b0e77ab8e424cab2badff0795e21e347
Garcia Aranda <jose_javier.garcia_aranda@nokia.com>
2019-05-28 13:04:59 +0200

    amazon works

--- application.py
+++ application.py
@@ -1,29 +1,29 @@
 from flask import Flask, request
 import dbManager, json
 
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.route('/')
+@application.route('/')
 def index():
-    return 'Server Works!'
+    return 'Server circle manager Works!'
 
 
-@app.route('/get_circle/<agent_id>')
+@application.route('/get_circle/<agent_id>')
 def get_circle(agent_id):
     circles = dbManager.get_circles_by_agent(agent_id)
     json_data = json.dumps(circles)
     return json_data
 
 
-@app.route('/get_circles')
+@application.route('/get_circles')
 def get_circles():
     circles = dbManager.get_all_circles()
     json_data = json.dumps(circles)
     return json_data
 
 
-@app.route('/create_circle', methods=['POST'])
+@application.route('/create_circle', methods=['POST'])
 def create_circles():
     json_data = request.get_json()
     circle = dbManager.create_circle(json_data)
@@ -31,14 +31,14 @@ def create_circles():
     return json_data
 
 
-@app.route('/attach_agent', methods=['POST'])
+@application.route('/attach_agent', methods=['POST'])
 def attach_agent():
     json_data = request.get_json()
     str_ret = dbManager.attach_agent_to_circle(json_data)
     return str_ret
 
 
-@app.route('/modify_circle', methods=['POST'])
+@application.route('/modify_circle', methods=['POST'])
 def modify_circle():
     json_data = request.get_json()
     updated_circle = dbManager.update_circle(json_data)
@@ -46,9 +46,11 @@ def modify_circle():
     return json_data
 
 
-@app.route('/get_circle_agents/<circle_id>')
+@application.route('/get_circle_agents/<circle_id>')
 def get_circle_agents(circle_id):
     circles = dbManager.get_agents_by_circle(circle_id)
     json_data = json.dumps(circles)
     return json_data
 
+if __name__ == '__main__':
+	application.run(host='0.0.0.0',port=80)
